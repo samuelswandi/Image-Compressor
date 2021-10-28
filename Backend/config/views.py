3fff6e92dfd47fa8a4a92f500ef273a2438d6bda
samuelswandi <16520414@std.stei.itb.ac.id>
2021-10-28 17:49:09 +0700

    Fixing frontend

--- Backend/config/views.py
+++ Backend/config/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the config index.")
+@api_view(['GET', 'POST'])
+
+def imageProcessing(request):
+
+    if request.method == 'GET':
+        return Response('hello')
+
+    elif request.method == 'POST':
+        return Response(request.body)

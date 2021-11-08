ad18854378c8d45f4a6e7770e12445f39ef4a7c0
samuelswandi <13520075@std.stei.itb.ac.id>
2021-11-09 02:20:09 +0700

    Resolving FE in BE

--- Backend/Configurations/views.py
+++ Backend/Configurations/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
+def main(request):
     return render(request, 'index.html')

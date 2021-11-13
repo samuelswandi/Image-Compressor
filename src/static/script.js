edea3dc7e3d33f4d132d43ce3ffd6d199ba0cfb9
samuelswandi <13520075@std.stei.itb.ac.id>
2021-11-13 22:59:12 +0700

    Done download compressed image

---
+++ src/static/script.js
@@ -0,0 +1,9 @@
+
+const downloadButton = document.querySelector("#downloadButton")
+const image = document.querySelector("#after");
+
+downloadButton.addEventListener('click', () => {
+    console.log("clicked");
+    const p = image.getAttribute('src');
+    saveAs(p, "compressedImage");
+})
